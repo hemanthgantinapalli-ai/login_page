7d983563a8ca3178cfcdd3355563e752185b2e96
Hemanth gantinapalli <hemanthgantinapalli@gmail.com>
2025-12-29 21:57:09 +0530

    Add files via upload

---
+++ page.js
@@ -0,0 +1,14 @@
+const form = document.getElementById("loginForm");
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  if (email === "" || password === "") {
+    alert("Please fill all fields");
+  } else {
+    alert("Login successful!");
+  }
+});
